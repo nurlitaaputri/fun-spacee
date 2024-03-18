dbe0de962d8c0cc477f1fca836736d24773d8231
nurlitaaputri <nurlitasgn87@gmail.com>
2024-03-18 13:48:52 +0700

    Uji Coba Hitung Pythagoras

---
+++ pythagoras.py
@@ -0,0 +1,8 @@
+# menghitung torema pythagoras
+def main():
+    a = float(input("Masukkan nilai a: "))
+    b = float(input("masukkan nilai b: "))
+    c = (a**2 + b**2)**0.5
+    print("Nilai c adalah: ", c)
+
+main()
